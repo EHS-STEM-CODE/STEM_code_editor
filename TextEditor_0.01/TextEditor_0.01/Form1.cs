858423898a012a0320428c0aeb53fef564215d51
xemjeff <jeff@xemory.com>
2015-10-14 16:26:39 -0400

    Changed name to MainForm.  Added FileEditor class to handle file editing details.

--- TextEditor_0.01/TextEditor_0.01/Form1.cs
+++ TextEditor_0.01/TextEditor_0.01/Form1.cs
@@ -9,13 +9,17 @@ using System.Windows.Forms;
 
 namespace TextEditor_0._01
 {
-    public partial class Form1 : Form  //probably not the best name for the form. Maybe MainForm or EditingForm.
+    public partial class MainForm : Form  //probably not the best name for the form. Maybe MainForm or EditingForm.
     {
         private String fileName;
+        private FileEditor fileEditor;
+
         private bool isFirstChange = true;
-        public Form1()
+        public MainForm()
         {
+            fileEditor = null;
             InitializeComponent();
+            OnNewFile();
         }
 
         private void closeToolStripMenuItem_Click(object sender, EventArgs e)
@@ -29,10 +33,10 @@ namespace TextEditor_0._01
             if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 System.IO.StreamReader openFile = new System.IO.StreamReader(openFileDialog1.FileName);
-                fileName = openFileDialog1.FileName;
+                fileEditor.SetPath(openFileDialog1.FileName);
                 textBox1.Text = openFile.ReadToEnd();
                 openFile.Close();
-                tabControl1.SelectedTab.Text = System.IO.Path.GetFileName(openFileDialog1.FileName);
+                tabControl1.SelectedTab.Text = fileEditor.TabLabel();//System.IO.Path.GetFileName(openFileDialog1.FileName);
                 isFirstChange = true;  //maybe something like fileIsDirty would be better.
             }
         }
@@ -64,13 +68,14 @@ namespace TextEditor_0._01
             if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 System.IO.StreamWriter saveFile = new System.IO.StreamWriter(saveFileDialog1.FileName);
-                fileName = saveFileDialog1.FileName;
+                fileEditor.SetPath(saveFileDialog1.FileName);
                 String[] lines = textBox1.Lines;
                 foreach (string line in lines)
                     saveFile.WriteLine(line);
                 saveFile.Close();
-                tabControl1.SelectedTab.Text = System.IO.Path.GetFileName(saveFileDialog1.FileName);
-                isFirstChange = true;
+                tabControl1.SelectedTab.Text = fileEditor.Path(); //System.IO.Path.GetFileName(saveFileDialog1.FileName);
+                fileEditor.SetDirty(false);
+                fileEditor.SetNew(false);
             }
         }
 
@@ -91,14 +96,74 @@ namespace TextEditor_0._01
 
         private void fileToolStripMenuItem1_Click(object sender, EventArgs e)
         {
+            OnNewFile();
+        }
+
+        private void OnNewFile()
+        {
             textBox1.Text = "";
-            tabControl1.SelectedTab.Text = "untitled";
-            fileName = "untitled";
+            fileEditor = new FileEditor();
+            tabControl1.SelectedTab.Text = fileEditor.TabLabel();
         }
 
         private void newToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
         }
+    }
+}
+
+public class FileEditor
+{
+    private string path;
+    private string shortName;
+    bool isDirty;
+    bool isNew;
+
+    public FileEditor()
+    {
+        isDirty = false;
+        isNew = true;
+        path = "";
+        shortName = "untitled";
+    }
+
+    public string TabLabel()
+    {
+        if (isDirty)
+            return shortName + "*";
+        else
+            return shortName;
+    }
+
+    public bool IsDirty()
+    {
+        return isDirty;
+    }
+
+    public bool IsNew()
+    {
+        return isNew;
+    }
+
+    public void SetDirty(bool b)
+    {
+        isDirty = b;
+    }
+
+    public void SetNew(bool b)
+    {
+        isNew = b;
+    }
+
+
+    public void SetPath(string _path)
+    {
+        path = _path;
+    }
+
+    public string Path()
+    {
+        return path;
     }
 }
