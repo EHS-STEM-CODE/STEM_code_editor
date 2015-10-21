ecaa4b63c5d96719d56ab61a0d2bb43bbd3131d3
xemjeff <jeff@xemory.com>
2015-10-21 18:11:26 -0400

    Added reference to ScintillaNET assembly.  Added code to set the keywords for javascript using the Lexer.Cpp setting in ScintillaNET.  Tested and working with basic Javascript.

--- TextEditor_0.02/TextEditor_0.02/Form1.Designer.cs
+++ TextEditor_0.02/TextEditor_0.02/Form1.Designer.cs
@@ -41,8 +41,8 @@
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.scintilla = new ScintillaNET.Scintilla();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.scintilla1 = new ScintillaNET.Scintilla();
             this.menuStrip1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -58,8 +58,8 @@
             this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(747, 33);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(660, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -72,43 +72,43 @@
             this.saveAsToolStripMenuItem,
             this.closeToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 29);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 22);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // newToolStripMenuItem
             // 
             this.newToolStripMenuItem.Name = "newToolStripMenuItem";
-            this.newToolStripMenuItem.Size = new System.Drawing.Size(156, 30);
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
             this.newToolStripMenuItem.Text = "New";
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(156, 30);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
             this.openToolStripMenuItem.Text = "Open";
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(156, 30);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
             this.saveToolStripMenuItem.Text = "Save";
             // 
             // saveAsToolStripMenuItem
             // 
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(156, 30);
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
             this.saveAsToolStripMenuItem.Text = "Save as";
             // 
             // closeToolStripMenuItem
             // 
             this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
-            this.closeToolStripMenuItem.Size = new System.Drawing.Size(156, 30);
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
             this.closeToolStripMenuItem.Text = "Close";
             // 
             // viewToolStripMenuItem
             // 
             this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
-            this.viewToolStripMenuItem.Size = new System.Drawing.Size(61, 29);
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 22);
             this.viewToolStripMenuItem.Text = "View";
             // 
             // toolsToolStripMenuItem
@@ -116,74 +116,76 @@
             this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.clearAllToolStripMenuItem});
             this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
-            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(67, 29);
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(48, 22);
             this.toolsToolStripMenuItem.Text = "Tools";
             // 
             // clearAllToolStripMenuItem
             // 
             this.clearAllToolStripMenuItem.Name = "clearAllToolStripMenuItem";
-            this.clearAllToolStripMenuItem.Size = new System.Drawing.Size(161, 30);
+            this.clearAllToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
             this.clearAllToolStripMenuItem.Text = "Clear All";
             // 
             // helpToolStripMenuItem
             // 
             this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-            this.helpToolStripMenuItem.Size = new System.Drawing.Size(61, 29);
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 22);
             this.helpToolStripMenuItem.Text = "Help";
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(0, 39);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabControl1.Location = new System.Drawing.Point(0, 25);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(747, 691);
+            this.tabControl1.Size = new System.Drawing.Size(498, 449);
             this.tabControl1.TabIndex = 1;
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.scintilla1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 29);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage1.Controls.Add(this.scintilla);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.tabPage1.Size = new System.Drawing.Size(739, 658);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.tabPage1.Size = new System.Drawing.Size(490, 423);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // scintilla
+            // 
+            this.scintilla.AutomaticFold = ScintillaNET.AutomaticFold.Show;
+            this.scintilla.Lexer = ScintillaNET.Lexer.Cpp;
+            this.scintilla.Location = new System.Drawing.Point(0, 0);
+            this.scintilla.Margin = new System.Windows.Forms.Padding(2);
+            this.scintilla.Name = "scintilla";
+            this.scintilla.Size = new System.Drawing.Size(489, 427);
+            this.scintilla.TabIndex = 2;
+            this.scintilla.UseTabs = false;
+            this.scintilla.Click += new System.EventHandler(this.scintilla1_Click);
+            // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 29);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.tabPage2.Size = new System.Drawing.Size(739, 658);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.tabPage2.Size = new System.Drawing.Size(490, 423);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // scintilla1
-            // 
-            this.scintilla1.Location = new System.Drawing.Point(0, 0);
-            this.scintilla1.Name = "scintilla1";
-            this.scintilla1.Size = new System.Drawing.Size(731, 655);
-            this.scintilla1.TabIndex = 2;
-            this.scintilla1.Text = "scintilla1";
-            this.scintilla1.UseTabs = false;
-            this.scintilla1.Click += new System.EventHandler(this.scintilla1_Click);
-            // 
             // mainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(747, 728);
+            this.ClientSize = new System.Drawing.Size(660, 536);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "mainWindow";
             this.Text = "CodeEditor_0.02";
             this.menuStrip1.ResumeLayout(false);
@@ -211,7 +213,7 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
-        private ScintillaNET.Scintilla scintilla1;
+        private ScintillaNET.Scintilla scintilla;
     }
 }
 
--- TextEditor_0.02/TextEditor_0.02/Form1.cs
+++ TextEditor_0.02/TextEditor_0.02/Form1.cs
@@ -8,6 +8,13 @@ using System.Text;
 using System.Windows.Forms;
 using ScintillaNET;
 
+/*
+ * this had some interesting application examples:
+ * https://github.com/jacobslusser/ScintillaNET/wiki
+ * 
+ * http://vipinkumaryadav.com/
+ */
+
 namespace TextEditor_0._02
 {
     public partial class mainWindow : Form
@@ -15,8 +22,67 @@ namespace TextEditor_0._02
         public mainWindow()
         {
             InitializeComponent();
-            scintilla1.Margins[0].Width = 32;   //Margins allow for up to 9999 lines -> Then it wraps around
-            ScintillaNET.ConfigurationManager.Language = "html";
+            scintilla.Margins[0].Width = 32;   //Margins allow for up to 9999 lines -> Then it wraps around
+            scintilla.Lexer = Lexer.Cpp;
+            Console.WriteLine(scintilla.DescribeKeywordSets());
+
+            scintilla.Enabled = true;
+
+            scintilla.Styles[Style.Default].Font = "Consolas";
+            scintilla.Styles[Style.Default].Size = 12;
+
+            // Configure the CPP (C#, javascript) lexer styles
+            scintilla.Styles[Style.Cpp.Default].ForeColor = Color.Silver;
+            scintilla.Styles[Style.Cpp.Comment].ForeColor = Color.FromArgb(0, 128, 0); // Green
+            scintilla.Styles[Style.Cpp.CommentLine].ForeColor = Color.FromArgb(0, 128, 0); // Green
+            scintilla.Styles[Style.Cpp.CommentLineDoc].ForeColor = Color.FromArgb(128, 128, 128); // Gray
+            scintilla.Styles[Style.Cpp.Number].ForeColor = Color.Olive;
+            scintilla.Styles[Style.Cpp.Word].ForeColor = Color.Blue;
+            scintilla.Styles[Style.Cpp.Word2].ForeColor = Color.Blue;
+            scintilla.Styles[Style.Cpp.String].ForeColor = Color.FromArgb(163, 21, 21); // Red
+            scintilla.Styles[Style.Cpp.Character].ForeColor = Color.FromArgb(163, 21, 21); // Red
+            scintilla.Styles[Style.Cpp.Verbatim].ForeColor = Color.FromArgb(163, 21, 21); // Red
+            scintilla.Styles[Style.Cpp.StringEol].BackColor = Color.Pink;
+            scintilla.Styles[Style.Cpp.Operator].ForeColor = Color.Purple;
+            scintilla.Styles[Style.Cpp.Preprocessor].ForeColor = Color.Maroon;
+            scintilla.Styles[Style.Cpp.GlobalClass].ForeColor = Color.Coral;
+            scintilla.Styles[Style.Cpp.CommentDocKeyword].ForeColor = Color.LightBlue;
+
+            scintilla.Styles[Style.BraceLight].BackColor = Color.LightGray;
+            scintilla.Styles[Style.BraceLight].ForeColor = Color.BlueViolet;
+            scintilla.Styles[Style.BraceBad].ForeColor = Color.Red;
+
+            //javascript keywords:  Style: Word
+            scintilla.SetKeywords(0, @"abstract arguments break case catch const continue debugger default delete do
+                                        else eval export extends false final finally for function goto if implements
+                                        import in instanceof interface let native new null package private protected 
+                                        public return short static super switch synchronized this throw throws 
+                                        transient true try typeof var void volatile while with yield");
+
+            //javascript secondary keywords (types)  - Style: Word2
+            scintilla.SetKeywords(1,  @"boolean byte char class double enum float int long");
+
+            //documentation keywords (for things like doxygen)
+            scintilla.SetKeywords(2, @"author authors brief");
+
+            //javascript global classes and typedef keywords Style: GlobalClass
+            scintilla.SetKeywords(3, @"Array Date eval hasOwnProperty Infinity isFinite isNaN isPrototypeOf length
+                                        Math NaN name Number Object prototype String toString undefined valueOf");
+            //C# keywords
+            /*
+            scintilla1.SetKeywords(0, @"abstract as base break case catch checked continue  
+                                      default delegate do else event explicit extern false 
+                                      finally fixed for foreach goto if implicit in interface 
+                                      internal is lock namespace new null object operator 
+                                      out override params private protected public readonly 
+                                      ref return sealed sizeof stackalloc switch this throw 
+                                      true try typeof unchecked unsafe using virtual while");
+
+            scintilla1.SetKeywords(1, @"bool byte char class const decimal double enum 
+                                      float int long sbyte short static string struct 
+                                      uint ulong ushort void");
+             */
+            
         }
        
       
@@ -36,6 +102,6 @@ namespace TextEditor_0._02
             String rich = Highlight(rawText);
             richTextBox1.Text = rich;
             //Or
-            richTextBox1.Text = Highlight(richTextBox1.Text);
+            richTextBox1.Text = Highlight(richTextBox1.Text);  // this is better. more consise. Easier to understand.
 
     */
