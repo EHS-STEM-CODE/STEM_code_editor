516a2230980a83d5db97a00bd8d8b432efff9456
anhdpham97@gmail.com <anhdpham97@gmail.com>
2015-11-18 20:00:33 -0800

    Basic Jint is working, can load a js file and run it now

---
+++ JSInterpreter/JSInterpreter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Jint;
+
+namespace JSInterpreter
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var engine = new Engine ().SetValue ("log", new Action<object> (Console.WriteLine));
+			var code = @"log('Test run successful')";
+			engine.Execute (code);
+			Console.Write("Enter the path of a file you want to execute -> ");
+			StreamReader openFile = new StreamReader ("C:\\Users\\John\\Desktop\\sample.txt");
+			var code1 = openFile.ReadToEnd ();
+			Console.WriteLine (code1);
+			engine.Execute (code1);
+			Console.ReadLine ();
+		}
+	}
+}
