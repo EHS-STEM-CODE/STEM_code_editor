bfe8f700eacd237d005822df16febcb46ae4180d
David-STEM <dstpierre@ccsuvt.org>
2015-12-11 21:16:17 -0500

    Added a two textbox server template

---
+++ jintServer/jintServer/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace jintServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            outputBox.Text = "Hello Derek! \r this text box is called outputBox";
+            codeBox.Text = "Here are your two textboxes \r this text box is called codeBox";
+        }
+
+    }
+}
